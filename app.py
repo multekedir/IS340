f952ce82f407e180d4bdbfa5279c158ca301aae4
Multezem Kedir <multekedir@users.noreply.github.com>
2019-03-09 17:48:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,36 @@
-from flask import Flask, flash, request, redirect, url_for
-import os
-#from werkzeug.utils import secure_filename
-import subprocess
-
-UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = set(['xml', 'xsl'])
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+from flask import Flask, make_response, request
 
 app = Flask(__name__)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# subprocess.getoutput("sudo xsltproc --stringparam schemaTypes ENTITY --stringparam dataTypes 'ID=CHAR(10);REF=CHAR(10);VARCHAR=VARCHAR(32);TIMESTAMP=TIMESTAMP;INT=INT;STR=TEXT;DATE=DATE;NAME=CHAR(20);PHONE=VARCHAR(12);BIT=BIT;' tableSchema_SQL.xsl Rideshare.xml")
-
-@app.route('/',methods=['GET','POST'])
-def home():
-    if request.method = 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-
-     return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+def transform(text_file_contents):
+    return text_file_contents.replace("=", ",")
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+@app.route('/')
+def form():
+    return """
+        <html>
+            <body>
+                <h1>Transform a file demo</h1>
+
+                <form action="/transform" method="post" enctype="multipart/form-data">
+                    <input type="file" name="data_file" />
+                    <input type="submit" />
+                </form>
+            </body>
+        </html>
+    """
+
+@app.route('/transform', methods=["POST"])
+def transform_view():
+    request_file = request.files['data_file']
+    if not request_file:
+        return "No file"
+
+    file_contents = request_file.stream.read().decode("utf-8")
+
+    result = transform(file_contents)
+
+    response = make_response(result)
+    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
+    return response
